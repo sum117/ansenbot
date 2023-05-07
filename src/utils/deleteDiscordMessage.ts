1f84a9219448b69477e82cc960df022b9e3d1b2f
sum117 <75037449+sum117@users.noreply.github.com>
2023-05-07 15:30:56 -0300

    fix: message.deletable super bugged.

--- src/utils/deleteDiscordMessage.ts
+++ src/utils/deleteDiscordMessage.ts
@@ -3,18 +3,14 @@ import type { Message } from "discord.js";
 export default function deleteDiscordMessage(message: Message, timeout: number): Promise<void> {
   return new Promise((resolve) => {
     setTimeout(() => {
-      if (message.deletable) {
-        message
-          .delete()
-          .then(() => {
-            resolve();
-          })
-          .catch((error) => {
-            console.error("Não foi possível deletar a mensagem: " + error);
-          });
-      } else {
-        resolve();
-      }
+      message
+        .delete()
+        .then(() => {
+          resolve();
+        })
+        .catch((error) => {
+          console.error("Não foi possível deletar a mensagem: " + error);
+        });
     }, timeout);
   });
 }
