9a0318f5469029b25e338951ffbabca087123870
sum117 <75037449+sum117@users.noreply.github.com>
2023-05-13 16:06:20 -0300

    fix: invalid _type error

--- src/lib/ansen-gpt/index.ts
+++ src/lib/ansen-gpt/index.ts
@@ -1,24 +1,8 @@
-import { ConversationalRetrievalQAChain, LLMChain, loadQAChain } from "langchain/chains";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { OpenAIChat } from "langchain/llms";
 import type { PineconeStore } from "langchain/vectorstores";
 
-import { CONDENSE_PROMPT, QA_PROMPT } from "./config/prompts";
-
 export const makeChain = (vectorstore: PineconeStore): ConversationalRetrievalQAChain => {
-  const questionGenerator = new LLMChain({
-    llm: new OpenAIChat({ temperature: 0 }),
-    prompt: CONDENSE_PROMPT,
-  });
-  const docChain = loadQAChain(
-    new OpenAIChat({
-      modelName: "gpt-4",
-      temperature: 0.5,
-    }),
-    { prompt: QA_PROMPT }
-  );
-  return new ConversationalRetrievalQAChain({
-    retriever: vectorstore.asRetriever(),
-    combineDocumentsChain: docChain,
-    questionGeneratorChain: questionGenerator,
-  });
+  const model = new OpenAIChat({ modelName: "gpt-4", temperature: 0.5 });
+  return ConversationalRetrievalQAChain.fromLLM(model, vectorstore.asRetriever());
 };
