d23dcbce70ec0a655a46f00944bb6c48f200f3c8
TeamWolfyta <kieran@wolfyta.moe>
2023-05-24 15:39:11 +0100

    refactor: tidy up merge image video functionality (#62)
    * refactor: tidy up merge image video functionality
    
    * fix: fix image generation starting without attachment but detected youtube url
    
    * refactor: add missing file save functionality
    
    Co-authored-by: sum117 <75037449+sum117@users.noreply.github.com>
    
    * chore: move ytdl-core from version to github link
    
    Co-authored-by: sum117 <75037449+sum117@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sum117 <75037449+sum117@users.noreply.github.com>

--- src/commands/util/MergeImageVideo.ts
+++ src/commands/util/MergeImageVideo.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import console from "console";
 import { AttachmentBuilder } from "discord.js";
 import type { ArgsOf } from "discordx";
 import { Discord, On } from "discordx";
@@ -8,6 +7,7 @@ import ffmpeg from "fluent-ffmpeg";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import type { videoInfo } from "ytdl-core";
 import ytdl from "ytdl-core";
 
 import { SPINNER_EMOJI } from "../../data/constants";
@@ -16,101 +16,137 @@ import handleError from "../../utils/handleError";
 import Queue from "../../utils/Queue";
 import resizeImage from "../../utils/resizeImage";
 
+interface GetPathsResult {
+  outputName: string;
+  cacheFolderPath: string;
+  outputPath: string;
+  imagePath: string;
+  audioFileStream: fs.WriteStream;
+  audioPath: string | Buffer;
+}
+
 @Discord()
 export class MergeImageVideo {
   private queue = new Queue();
+
   @On({ event: "messageCreate" })
-  main([message]: ArgsOf<"messageCreate">): void {
+  public main([message]: ArgsOf<"messageCreate">): void {
     this.queue.enqueue(async () => {
       try {
         if (message.author.bot) {
           return;
         }
-        const now = Date.now();
 
-        const ffmpegPathResolved = ffmpegPath;
-        const link = message.content;
-        if (!ytdl.validateURL(link) || !ffmpegPathResolved) {
+        if (!ffmpegPath) {
           return;
         }
-        const loading = await message.channel.send(`${SPINNER_EMOJI} Gerando vídeo...`);
-        const info = await ytdl.getInfo(link);
 
-        const imageLink = message.attachments.first()?.url;
+        const audioLink = message.content;
+        if (!ytdl.validateURL(audioLink)) {
+          return;
+        }
+
+        const imageAttachment = message.attachments.first();
+        if (!imageAttachment) {
+          return;
+        }
+
+        const imageLink = imageAttachment.url;
         const imageSuffix = imageLink?.split("/").pop();
-        if (!imageSuffix || !imageLink) {
+        if (!imageSuffix) {
           return;
         }
 
-        const outputName = "output" + now + ".mp4";
-        const cacheFolderPath = ensureCacheFolder();
-        const outputPath = path.join(cacheFolderPath, outputName);
-        const imagePath = path.join(cacheFolderPath, now + imageSuffix);
-        const audioPath = fs.createWriteStream(path.join(cacheFolderPath, "audio" + now + ".mp3"));
-        const streamPath = audioPath.path;
-        if (typeof streamPath !== "string") {
+        const paths = this.getPaths(imageSuffix);
+        if (typeof paths.audioPath !== "string") {
           return;
         }
 
-        await axios.get(imageLink, { responseType: "arraybuffer" }).then((res) => {
-          resizeImage(res.data).then((buffer) => {
-            fs.writeFileSync(imagePath, buffer);
-          });
-        });
+        const loading = await message.channel.send(`${SPINNER_EMOJI} Gerando vídeo...`);
+        const info = await ytdl.getInfo(audioLink);
 
-        ytdl(link, { quality: "highestaudio" })
-          .pipe(audioPath)
-          .on("finish", () => {
-            ffmpeg()
-              .setFfmpegPath(ffmpegPathResolved)
-              .input(imagePath)
-              .loop(info.videoDetails.lengthSeconds)
-              .input(streamPath)
-              .audioCodec("aac")
-              .audioBitrate("192k")
-              .videoCodec("libx264")
-              .videoFilters(["scale='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p"])
-              .outputOptions("-tune stillimage")
-              .outputOptions("-shortest")
-              .outputOptions("-movflags +faststart")
-              .on("end", () => {
-                try {
-                  audioPath.close();
-                  fs.unlinkSync(streamPath);
-                  const attachment = new AttachmentBuilder(outputPath).setName(outputName);
-                  message
-                    .reply({
-                      files: [attachment],
-                    })
-                    .then(() => {
-                      try {
-                        deleteDiscordMessage(loading, 0).catch(console.error);
-                        fs.unlinkSync(imagePath);
-                        fs.unlinkSync(outputPath);
-                      } catch (error) {
-                        handleError(message, error);
-                      }
-                    });
-                } catch (error) {
-                  handleError(message, error);
-                }
-              })
-              .on("error", (err) => {
-                try {
-                  console.log(err);
-                  audioPath.close();
-                  fs.unlinkSync(streamPath);
-                  fs.unlinkSync(imagePath);
-                  fs.unlinkSync(outputPath);
-                } catch (error) {
-                  handleError(message, error);
-                }
-              })
-              .saveToFile(outputPath);
-          });
+        await this.downloadDiscordImage(imageLink, paths.imagePath);
+        await this.downloadYoutubeAudio(audioLink, paths.audioFileStream);
+        await this.mergeYoutubeAudioAndImage(
+          paths.imagePath,
+          info,
+          paths.audioPath,
+          paths.outputPath
+        );
+
+        await deleteDiscordMessage(loading, 0);
+
+        const attachment = new AttachmentBuilder(paths.outputPath).setName(paths.outputName);
+
+        await message.reply({ files: [attachment] });
+
+        paths.audioFileStream.close();
+        fs.unlinkSync(paths.audioPath);
+        fs.unlinkSync(paths.imagePath);
+        fs.unlinkSync(paths.outputPath);
       } catch (error) {
         handleError(message, error);
       }
+    });
+  }
+
+  private getPaths(imageSuffix: string): GetPathsResult {
+    const now = Date.now();
+    const outputName = "output" + now + ".mp4";
+    const cacheFolderPath = ensureCacheFolder();
+    const outputPath = path.join(cacheFolderPath, outputName);
+    const imagePath = path.join(cacheFolderPath, now + imageSuffix);
+    const audioFileStream = fs.createWriteStream(
+      path.join(cacheFolderPath, "audio" + now + ".opus")
+    );
+    const audioPath = audioFileStream.path;
+    return { outputName, cacheFolderPath, outputPath, imagePath, audioFileStream, audioPath };
+  }
+
+  private downloadDiscordImage(link: string, imagePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(link, { responseType: "arraybuffer" })
+        .then((res) => {
+          resizeImage(res.data)
+            .then((buffer) => fs.writeFile(imagePath, buffer, {}, () => resolve()))
+            .catch(reject);
+        })
+        .catch(reject);
+    });
+  }
+
+  private downloadYoutubeAudio(audioLink: string, audioFileStream: fs.WriteStream): Promise<void> {
+    return new Promise((resolve, reject) => {
+      ytdl(audioLink, { quality: "highestaudio" })
+        .pipe(audioFileStream)
+        .on("finish", resolve)
+        .on("error", reject);
+    });
+  }
+
+  private mergeYoutubeAudioAndImage(
+    imagePath: string,
+    info: videoInfo,
+    audioPath: string,
+    outputPath: string
+  ): Promise<void> {
+    return new Promise((resolve, reject) => {
+      ffmpeg()
+        .setFfmpegPath(ffmpegPath!)
+        .input(imagePath)
+        .loop(info.videoDetails.lengthSeconds)
+        .input(audioPath)
+        .audioCodec("aac")
+        .audioBitrate("192k")
+        .videoCodec("libx264")
+        .videoFilters(["scale='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p"])
+        .outputOptions("-tune stillimage")
+        .outputOptions("-shortest")
+        .outputOptions("-movflags +faststart")
+        .on("end", resolve)
+        .on("error", reject)
+        .saveToFile(outputPath);
     });
   }
 }
